d15ce0c51a70a96df16adace5749f9fcc6d44316
Felix Thierfelder <felixthier@github.com>
2023-08-30 16:11:27 +0200

    add toggle themes catppuccin and nord

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -6,8 +6,7 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "catppuccin",
-  theme_toggle = { "onedark", "one_light" },
-
+  theme_toggle = { "catppuccin", "nord" },
   hl_override = highlights.override,
   hl_add = highlights.add,
 }
